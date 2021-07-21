03d5512f291cb7321a25eeb390aa7599bb6a9825
wodhks08 <ghks2047@naver.com>
2021-07-21 23:55:26 +0900

    modbus poll update

--- routes/settings.js
+++ routes/settings.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 // modbus 제어, bacnet제어, database제어
 // 3가지 모두 pm2를 사용하여 제어를 진행한다.
-
 router.get('/restart_only/:module', function (req, res, next) {
     console.log(req.params.module)
     Handler.restart_only(req.params.module)

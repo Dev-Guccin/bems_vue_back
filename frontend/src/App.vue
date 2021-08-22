7c085e8d571af3aa2f546050352d204e6c5098e4
sungyeon-0975 <sustain98@naver.com>
2021-08-22 16:57:37 +0900

    home layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,68 @@
 <template>
-    <v-app id="inspire">
+    <div>
+    <!-- Image and text -->
+        <b-navbar variant="dark" type="dark" class="px-3">
+            <b-navbar-brand href="#">
+            <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="Kitten">
+            4m-Bems
+            </b-navbar-brand>
+        </b-navbar>
+        <b-container fluid class="m-0 p-0">
+            <b-row class="">
+                <b-col cols="2" class="p-0">
+                    <b-nav vertical class="vh-100 ">
+                        <v-list color="transparent p-0">
+                            <v-list-item 
+                            v-for="item in side" 
+                            :key="item.text"
+                                link router :to="{name: item.router}">
+                                <v-list-item-icon>
+                                    <v-icon v-text="item.icon"></v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ item.text }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+
+                            <v-divider class="my-2"></v-divider>
+
+                            <v-list-item link 
+                            router :to="{name: setting[0].router}">
+                                <v-list-item-icon >
+                                    <v-icon v-text="setting[0].icon"></v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{setting[0].text}}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </b-nav>
+                </b-col>
+                <b-col>
+                    <div>
+                        <router-view/> 
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
+        
+        <!-- <b-sidebar class="fixed" id="sidebar-1" title="Sidebar" shadow>
+        <div class="px-3 py-2">
+            <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        </div>
+        </b-sidebar> -->
+
+    </div>
+</template>
+    <!-- <v-app id="inspire">
         <v-app-bar app color="white" flat>
             <v-container class="py-0 fill-height">
                 <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
@@ -51,15 +114,14 @@
     
                     <v-col>
                         <v-sheet min-height="70vh" rounded="lg">
-                        <!-- 메인 -->
-                        <router-view/>
+                        메인 -->
+                        <!-- <router-view/>
                         </v-sheet>
                     </v-col>
                 </v-row>
             </v-container>
         </v-main>
-    </v-app>
-</template>
+    </v-app> --> 
 
 <script>
 export default {
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,46 +1,122 @@
 <template>
   <div class="home">
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
+    <p> > 건물 에너지 사용 현황</p>
+    <b-container class="px-0 mx-auto" style="height: 100px; ">
+      <b-row>
+        <b-col cols="">
+          <b-card
+            height="400px"
+            style="height:400px;"
+            header="featured"
+            header-tag="header"
+          >
+          <b-card-body>
+            <!-- <line-chart :chart-data="datacollection" :height="400"></line-chart> -->
+          </b-card-body>
+          </b-card>
+            
+      <!-- <button @click="fillData()">Randomize</button> -->
+        </b-col>
+        <b-col cols="2">
+          <b-card
+            height="400px"
+            style="height:400px;"
+            header="featured"
+            header-tag="header"
+          >
+          <b-card-body class="">
+          </b-card-body>
+          </b-card>
+            
+        </b-col>
+        <b-col cols="2">
+          <b-card
+            height="400px"
+            style="height:400px;"
+            header="featured"
+            header-tag="header"
+          >
+          <b-card-body>
+          </b-card-body>
+          </b-card>
+            
+        </b-col>
+      </b-row>
+      
+      <b-row>
+        <b-col cols="8">
+          <b-card
+            height="400px"
+            style="height:400px;"
+            header="featured"
+            header-tag="header"
+          >
+          <b-card-body>
+            <!-- <line-chart :chart-data="datacollection" :height="400"></line-chart> -->
+          </b-card-body>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+          <b-card
+            height="400px"
+            style="height:400px;"
+            header="featured"
+            header-tag="header"
+          >
+          <b-card-body>
+            <!-- <line-chart :chart-data="datacollection" :height="400"></line-chart> -->
+          </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+    
+    
   </div>
 </template>
 
 <script>
-import LineChart from '../components/charts/LineChart'
+// import LineChart from '../components/charts/LineChart'   
 
-export default {
-  name: 'Home',
-  components: {
-    LineChart
-  },
-  data(){
-    return{
-      datacollection: null
-    }
-  },
-  mounted(){
-    this.fillData()
-  },
-  methods:{
-     fillData () {
-        this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
-          datasets: [
-            {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }, {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }
-          ]
-        }
-     },
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-    }
-  }
-}
+// export default {
+//   name: 'Home',
+//   components: {
+//     LineChart
+//   },
+//   data(){
+//     return{
+//       datacollection: null,
+//       chartStyles: {
+//         height : '400px',
+//         position: 'relative'
+//       }
+
+//     }
+//   },
+//   mounted(){
+//     this.fillData()
+//   },
+//   methods:{
+//      fillData () {
+//         this.datacollection = {
+//           labels: [this.getRandomInt(), this.getRandomInt()],
+//           datasets: [
+//             {
+//               label: 'Data One',
+//               backgroundColor: '#f87979',
+//               data: [this.getRandomInt(), this.getRandomInt()]
+//             }, {
+//               label: 'Data One',
+//               backgroundColor: '#f87979',
+//               data: [this.getRandomInt(), this.getRandomInt()]
+//             }
+//           ]
+//         },
+//         this.option
+//      },
+//       getRandomInt () {
+//         return (Math.floor(Math.random() * (50 - 5 + 1)) + 5)%10
+//     }
+//   }
+// }
 </script>

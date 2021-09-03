e4db73b1ce4a8855006cc3dccfa4b0c9c45b19f2
wodhks08 <ghks2047@naver.com>
2021-09-04 01:56:22 +0900

    차트 적용 완료

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -99,7 +99,6 @@
 <script>
 import Chart from "chart.js";
 
-
 export default {
     data() {
         return {
@@ -123,35 +122,35 @@ export default {
             this.$refs.lineChart = new Chart(chartCtx, {
                 type: 'bar',
                 data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
+                  labels: [1,2,3],
+                  datasets: [
+                    {
+                      label: 'Dataset 1',
+                      data: [[0,2],[0,3]],
+                      backgroundColor: "#f67019",
+                    },
+                    {
+                      label: 'Dataset 2',
+                      data: [3,2,2.2],
+                      backgroundColor:   '#4dc9f6',                    },
+                    {
+                      label: 'Dataset 3',
+                      data: [2,3.1,2],
+                      backgroundColor: '#acc236',
+                    },
+                  ]
                 },
                 options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+
+                  responsive: true,
+                  scales: {
+                    x:{
+                      stacked: true,
+                    },
+                    y: {
+                      stacked: true,
                     }
+                  }
                 }
             })
         }
